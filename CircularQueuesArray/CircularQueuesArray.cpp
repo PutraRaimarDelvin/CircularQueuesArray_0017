2a5dada17acc5f19a86fd0611a2c850fd8d71378
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-05-30 19:04:46 +0700

    9

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -2,19 +2,22 @@
 using namespace std;
 
 
-        else {
-            while (FRONT_position <= max - 1) {
-                cout << queue_array[FRONT_position] << "   ";
-                FRONT_position++;
-            }
-            FRONT_position = 0;
 
-            while (FRONT_position <= REAR_position) {
-                cout << queue_array[FRONT_position] << "   ";
-                FRONT_position++;
-            }
+int main() {
+    Queues q;
+    char ch;
+
+    while (true) {
+        try {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
             cout << endl;
-        }
-    }
-};
 
+    }
+    return 0;
+}
