386b490d2087464b90f4138df4e11290ccf68605
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-05-30 19:06:02 +0700

    12

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -1,43 +1,39 @@
 #include <iostream>
 using namespace std;
 
+class Queues {
+    int FRONT, REAR, max = 5;
+    int queue_array[5];
 
-int main() {
-    Queues q;
-    char ch;
-
-    while (true) {
-        try {
-            cout << "Menu" << endl;
-            cout << "1. Implement insert operation" << endl;
-            cout << "2. Implement delete operation" << endl;
-            cout << "3. Display values" << endl;
-            cout << "4. Exit" << endl;
-            cout << "Enter your choice (1-4): ";
-            cin >> ch;
-            cout << endl;
-
-            switch (ch)
-            {
-            case '1':
-                q.insert();
-                break;
-            case '2':
-                q.remove();
-                break;
-            case '3':
-                q.display();
-                break;
-            case '4':
-                return 0;
-            default:
-                cout << "Invalid option!!" << endl;
-                break;
-            }
-        }
-        catch (exception& e) {
-            cout << "Check for the values entered." << endl;
-        }
+public:
+    Queues() {
+        FRONT = -1;
+        REAR = -1;
     }
-    return 0;
-}
+    void insert() {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        //Cek apakah antrian penuh atau tidak
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+
+        //Cek apakah antrian kosong
+        if (FRONT == -1) {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    
