d1c5d13cec6b57e746162d5a85def7a20623e0d5
PutraRaimarDelvin <delvinputra805@gmail.com>
2024-05-30 19:04:23 +0700

    8

--- CircularQueuesArray/CircularQueuesArray.cpp
+++ CircularQueuesArray/CircularQueuesArray.cpp
@@ -1,59 +1,20 @@
 #include <iostream>
 using namespace std;
 
-class Queues {
-    int FRONT, REAR, max = 5;
-    int queue_array[5];
 
-public:
-    Queues() {
-        FRONT = -1;
-        REAR = -1;
-    } 
-    void insert() {
-        int num;
-        cout << "Enter a number: ";
-        cin >> num;
-        cout << endl;
-
-        //Cek apakah antrian penuh atau tidak
-        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-            cout << "\nQueue overflow\n";
-            return;
-        }
-
-        //Cek apakah antrian kosong
-        if (FRONT == -1) {
-            FRONT = 0;
-            REAR = 0;
-        }
         else {
-            if (REAR == max - 1)
-                REAR = 0;
-            else
-                REAR = REAR + 1;
-        }
-        queue_array[REAR] = num;
-    }
+            while (FRONT_position <= max - 1) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
 
-    void remove() {
-        //Cek apakkah antrian kosong
-        if (FRONT == -1) {
-            cout << "Queue underflow\n";
-            return;
-        }
-        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
-
-        if (FRONT == REAR) {
-            FRONT = -1;
-            REAR = -1;
-        }
-        else {
-            if (FRONT == max - 1)
-                FRONT = 0;
-            else
-                FRONT = FRONT + 1;
+            while (FRONT_position <= REAR_position) {
+                cout << queue_array[FRONT_position] << "   ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
     }
+};
 
-   
